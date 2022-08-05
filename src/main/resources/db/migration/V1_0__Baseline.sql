72b32b24dc8b94895d806647de30a80e4debaa15
Harmelodic <matt@harmelodic.com>
2022-08-05 19:19:03 +0200

    Remove backticks since postgres doesn't support them

--- src/main/resources/db/migration/V1_0__Baseline.sql
+++ src/main/resources/db/migration/V1_0__Baseline.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS `post`
+CREATE TABLE IF NOT EXISTS post
 (
-    `id`           uuid         NOT NULL,
-    `title`        VARCHAR(255) NOT NULL,
-    `route`        VARCHAR(255) NOT NULL,
-    `date_posted`  bigint       NOT NULL,
-    `last_updated` bigint       NOT NULL,
-    `file_name`    VARCHAR(255) NOT NULL,
-    `category`     VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`)
+    id           uuid         NOT NULL,
+    title        VARCHAR(255) NOT NULL,
+    route        VARCHAR(255) NOT NULL,
+    date_posted  bigint       NOT NULL,
+    last_updated bigint       NOT NULL,
+    file_name    VARCHAR(255) NOT NULL,
+    category     VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
