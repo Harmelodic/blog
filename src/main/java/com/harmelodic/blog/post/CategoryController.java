79d0b54e97d0c26c4ac2723b442275275e15d479
Matt Smith <matt@harmelodic.com>
2022-08-09 17:21:11 +0200

    Enable Cross Origin

--- src/main/java/com/harmelodic/blog/post/CategoryController.java
+++ src/main/java/com/harmelodic/blog/post/CategoryController.java
@@ -1,5 +1,6 @@
 package com.harmelodic.blog.post;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/category")
+@CrossOrigin
 public class CategoryController {
 
     private final CategoryService categoryService;
--- src/main/java/com/harmelodic/blog/post/PostController.java
+++ src/main/java/com/harmelodic/blog/post/PostController.java
@@ -1,5 +1,6 @@
 package com.harmelodic.blog.post;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/post")
+@CrossOrigin
 public class PostController {
 
     private final PostService postService;
