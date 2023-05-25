41fa7e982d4b1fed9fb1c29a219738adaad9b5a2
Harmelodic <matt@harmelodic.com>
2023-05-26 01:35:13 +0200

    BlogPost integration pretty much there

--- src/main/java/com/harmelodic/blog/post/contentful/ContentfulClient.java
+++ src/main/java/com/harmelodic/blog/post/contentful/ContentfulClient.java
@@ -1,37 +1,76 @@
 package com.harmelodic.blog.post.contentful;
 
-import com.contentful.java.cda.CDAClient;
-import com.contentful.java.cda.CDAEntry;
 import com.harmelodic.blog.post.BlogPost;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
-// https://contentful.github.io/contentful.java/
 @Component
 public class ContentfulClient {
 
-    final String LOCALE = "en-GB";
+    WebClient client;
+    String token;
+    String space;
+    String environment;
 
-    final CDAClient client;
-
-    ContentfulClient(CDAClient client) {
-        this.client = client;
+    ContentfulClient(WebClient.Builder builder,
+                     @Value("${contentful.token}") String token,
+                     @Value("${contentful.space}") String space,
+                     @Value("${contentful.environment}") String environment) {
+        this.client = builder.baseUrl("https://cdn.contentful.com").build();
+        this.token = token;
+        this.space = space;
+        this.environment = environment;
     }
 
     List<BlogPost> fetchAllBlogPosts() {
-        return client.fetch(CDAEntry.class)
-                .withContentType("blogPost")
-                .orderBy("-sys.createdAt")
-                .all()
-                .entries()
-                .values()
-                .stream()
-                .map(cdaEntry -> new BlogPost(
-                        cdaEntry.id(),
-                        cdaEntry.getField(LOCALE, "title"),
-                        cdaEntry.getField(LOCALE, "content")
-                ))
-                .toList();
+        ContentfulBlogPostResponseBody response = client.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/spaces/{space}/environments/{environment}/entries")
+                        .queryParam("access_token", List.of(token))
+                        .queryParam("limit", 100)
+                        .queryParam("order", "-sys.createdAt")
+                        .queryParam("sys.contentType.sys.id", "blogPost")
+                        .build(Map.of(
+                                "space", space,
+                                "environment", environment
+                        )))
+                .exchangeToMono(clientResponse -> clientResponse.bodyToMono(ContentfulBlogPostResponseBody.class))
+                .block();
+
+
+        if (response != null) {
+            return response.items()
+                    .stream()
+                    .map(contentfulBlogPost -> new BlogPost(
+                            contentfulBlogPost.sys().id(),
+                            contentfulBlogPost.fields().title(),
+                            contentfulBlogPost.fields().content()
+                    ))
+                    .toList();
+        }
+        else {
+            return Collections.emptyList();
+        }
+    }
+
+    record ContentfulBlogPostResponseBody(List<ContentfulEntry> items) {
+    }
+
+    record ContentfulEntry(ContentfulSys sys,
+                           ContentfulBlogPost fields) {
+    }
+
+    record ContentfulSys(String id) {
+    }
+
+    record ContentfulBlogPost(Integer id,
+                              String title,
+                              String publishedOn,
+                              String content) {
     }
 }
--- src/test/java/com/harmelodic/blog/post/contentful/ContentfulClientTest.java
+++ src/test/java/com/harmelodic/blog/post/contentful/ContentfulClientTest.java
@@ -2,23 +2,25 @@ package com.harmelodic.blog.post.contentful;
 
 import com.harmelodic.blog.post.BlogPost;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest(classes = {ContentfulClient.class, SpringContentfulConfig.class})
 class ContentfulClientTest {
 
-    @Autowired
     ContentfulClient client;
 
     @Test
     void fetchAll() {
+        client = new ContentfulClient(WebClient.builder(),
+                "",
+                "ei54cjka6crw",
+                "master");
         List<BlogPost> posts = client.fetchAllBlogPosts();
 
+        posts.forEach(blogPost -> System.out.println(blogPost.id() + " - " + blogPost.title()));
         assertNotNull(posts);
     }
 }
